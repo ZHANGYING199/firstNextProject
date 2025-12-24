405c1d5f5c68498fa8f9fcd824afd8bf05229c95
[200~ <2832961898@qq.com>
2025-12-24 21:36:51 +0800

    修改文件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Layout from "@/components/layout";
 import Content from "@/modules/home/content";
 
 export default function Home() {
+  const apiUrl = process.env.MONGODB_URI;
+  console.log(apiUrl)
   return (
     <Layout>
       <Suspense fallback={<div>Loading...</div>}>
